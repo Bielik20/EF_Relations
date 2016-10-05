c97665681bc0a29f24f210c6a84e6dcbf4ba8045
Damian Bielecki <db460597@wmservice.corpnet1.com>
2016-10-05 13:32:28 +0200

    Add Movie and Genre models

---
+++ EF_Relations/Models/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF_Relations.Models
+{
+    public class Genre
+    {
+        public Genre()
+        {
+            this.Movies = new HashSet<Movie>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ EF_Relations/Models/Movies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF_Relations.Models
+{
+    public class Movie
+    {
+        public Movie()
+        {
+            this.Genres = new HashSet<Genre>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int RunningTime { get; set; }
+
+        public virtual ICollection<Genre> Genres { get; set; }
+    }
+}
