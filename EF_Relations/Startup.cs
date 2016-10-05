d6cbed680d6877af43f487d23474ab34ae96ec92
db460597 <db460597@POZWL15H63790>
2016-10-05 13:10:40 +0200

    Add project files.

---
+++ EF_Relations/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EF_Relations.Startup))]
+namespace EF_Relations
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
