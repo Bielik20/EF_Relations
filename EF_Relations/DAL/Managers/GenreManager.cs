ea5a3df118bf947140da9ad8c389cb8fba9879d0
Damian Bielecki <db460597@wmservice.corpnet1.com>
2016-10-06 12:02:49 +0200

    Add GenreManager

---
+++ EF_Relations/DAL/Managers/GenreManager.cs
@@ -0,0 +1,35 @@
+﻿using EF_Relations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF_Relations.DAL.Managers
+{
+    public static class GenreManager
+    {
+        public static List<Genre> GetAll()
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                return context.Genres.OrderBy(x => x.Name).ToList();
+            }
+        }
+
+        public static Genre GetByID(int id)
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                return context.Genres.Where(x => x.Id == id).First();
+            }
+        }
+
+        public static List<Genre> GetForMovie(int id)
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                return context.Movies.Where(x => x.Id == id).First().Genres.ToList();
+            }
+        }
+    }
+}
