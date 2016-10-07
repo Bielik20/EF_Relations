108cc9fa24fe657f80a995a0dd2cfba84aa4eb73
Damian Bielecki <db460597@wmservice.corpnet1.com>
2016-10-07 12:47:58 +0200

    Added CreateMovieVM

---
+++ EF_Relations/ViewModels/MovieVM/CreateMovieVM.cs
@@ -0,0 +1,25 @@
+﻿using EF_Relations.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EF_Relations.ViewModels.MovieVM
+{
+    public class CreateMovieVM
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DisplayName("Release Date: ")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        public int RunningTime { get; set; }
+
+        public List<CheckBoxListItem> Genres { get; set; } = new List<CheckBoxListItem>();
+    }
+}
